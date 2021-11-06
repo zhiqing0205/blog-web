c0470c429e1b48b78d54750375c52bcd459bee66
Zhiqing ZHong <1775840762@qq.com>
2021-11-07 01:41:34 +0800

    集成图标组件 npm install @ant-design/icons-vue --save

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@
  * @Author: Zhiqing Zhong
  * @Date: 2021-11-06 19:33:53
  * @LastEditors: Zhiqing Zhong
- * @LastEditTime: 2021-11-06 20:51:28
+ * @LastEditTime: 2021-11-07 01:39:07
  */
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -12,6 +12,13 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
 
+//全局使用图标
+const icons : any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
