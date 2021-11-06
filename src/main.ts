334b2ce7ac813060a735653b208ba2be7990724e
Zhiqing ZHong <1775840762@qq.com>
2021-11-06 20:54:38 +0800

    集成Ant Design Vue，加入按钮示例

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Author: Zhiqing Zhong
+ * @Date: 2021-11-06 19:33:53
+ * @LastEditors: Zhiqing Zhong
+ * @LastEditTime: 2021-11-06 20:51:28
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Antd).mount('#app');
+
