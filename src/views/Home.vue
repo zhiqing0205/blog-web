ebefb4206b661bc70ac22593255217cc03cfc324
Zhiqing ZHong <1775840762@qq.com>
2021-11-07 00:06:05 +0800

    去除HelloWorld组件及其相关代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,13 +61,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from "vue";// @ is an alias to /src
 
 export default defineComponent({
 	name: "Home",
 	components: {
-		HelloWorld,
 	},
 });
 </script>
