816a6336f548614317caecee7c0224ba86aad751
Zhiqing ZHong <1775840762@qq.com>
2021-11-08 11:49:26 +0800

    修改电子书管理页面布局

--- src/views/admin/admin-ebook.vue
+++ src/views/admin/admin-ebook.vue
@@ -4,11 +4,22 @@
  * @Author: Zhiqing Zhong
  * @Date: 2021-11-08 11:37:28
  * @LastEditors: Zhiqing Zhong
- * @LastEditTime: 2021-11-08 11:37:28
+ * @LastEditTime: 2021-11-08 11:48:48
 -->
 
 <template>
-	<div class="about">
-		<h1>admin-ebook</h1>
-	</div>
+	<a-layout>
+		<a-layout-content
+			:style="{
+				background: '#fff',
+				padding: '24px',
+				margin: 0,
+				minHeight: '280px',
+			}"
+		>
+			<div class="about">
+				<h1>admin-ebook</h1>
+			</div>
+		</a-layout-content>
+	</a-layout>
 </template>
