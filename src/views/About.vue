182e034c7871cb1bde4432d0363e577bce70229a
Zhiqing ZHong <1775840762@qq.com>
2021-11-09 18:27:23 +0800

    修改about页样式

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,10 +4,23 @@
  * @Author: Zhiqing Zhong
  * @Date: 2021-11-06 19:33:53
  * @LastEditors: Zhiqing Zhong
- * @LastEditTime: 2021-11-06 19:49:43
+ * @LastEditTime: 2021-11-09 18:18:38
 -->
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
-	</div>
+	<a-layout>
+		<a-layout>
+			<a-layout-content
+				:style="{
+					background: '#fff',
+					padding: '24px',
+					margin: 0,
+					minHeight: '280px',
+				}"
+			>
+				<div class="about">
+					<h1>This is an about page</h1>
+				</div>
+			</a-layout-content>
+		</a-layout>
+	</a-layout>
 </template>
