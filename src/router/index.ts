60d8f5ec2e8be5dae40df328176e1e062c507e46
Zhiqing Zhong <1775840762@qq.com>
2022-01-09 23:50:44 +0800

    初步复制生存文档管理代码，并没有根据id进行页面跳转

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,14 @@
  * @Author: Zhiqing Zhong
  * @Date: 2021-11-06 19:33:53
  * @LastEditors: Zhiqing Zhong
- * @LastEditTime: 2022-01-09 19:14:04
+ * @LastEditTime: 2022-01-09 23:44:36
  */
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/home.vue";
 import About from "../views/about.vue";
 import AdminEbook from "../views/admin/admin-ebook.vue";
 import AdminCategory from "../views/admin/admin-category.vue";
+import AdminDoc from "../views/admin/admin-doc.vue";
 
 const routes: Array<RouteRecordRaw> = [
 	{
@@ -32,6 +33,11 @@ const routes: Array<RouteRecordRaw> = [
 		path: "/admin/category",
 		name: "AdminCategory",
 		component: AdminCategory,
+	},
+    {
+		path: "/admin/doc",
+		name: "AdminDoc",
+		component: AdminDoc,
 	},
 ];
 
---
+++ src/views/admin/admin-doc.vue
@@ -0,0 +1,255 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Author: Zhiqing Zhong
+ * @Date: 2021-11-08 11:37:28
+ * @LastEditors: Zhiqing Zhong
+ * @LastEditTime: 2022-01-09 23:42:16
+-->
+
+<template>
+	<a-layout>
+		<a-layout-content
+			:style="{
+				background: '#fff',
+				padding: '24px',
+				margin: 0,
+				minHeight: '280px',
+			}"
+		>
+			<a-form layout="inline" :model="search">
+				<a-form-item>
+					<a-button type="primary" @click="handleQuery()"> 查询 </a-button>
+				</a-form-item>
+				<a-form-item>
+					<a-button type="primary" @click="add" size="lager">增加</a-button>
+				</a-form-item>
+			</a-form>
+
+			<a-table
+				:columns="columns"
+				:data-source="level"
+				:row-key="(record) => record.id"
+				:loading="loading"
+				:pagination="false"
+			>
+				<template #headerCellCover>
+					<span>
+						<smile-outlined />
+						封面
+					</span>
+				</template>
+
+				<template #bodyCell="{ column, record }">
+					<template v-if="column.key === 'action'">
+						<span>
+							<a-space
+								><a-button type="primary" @click="edit(record)">编辑</a-button>
+								<a-popconfirm
+									title="是否删除，删除后不可恢复"
+									ok-text="是"
+									cancel-text="否"
+									@confirm="handleDelete(record.id)"
+								>
+									<a-button type="primary" danger>删除</a-button>
+								</a-popconfirm></a-space
+							>
+						</span>
+					</template>
+
+					<template v-else-if="column.key === 'cover'">
+						<img v-if="record.cover" :src="record.cover" alt="avator" />
+					</template>
+				</template>
+			</a-table>
+		</a-layout-content>
+	</a-layout>
+
+	<a-modal
+		v-model:visible="modalVisible"
+		title="文档表单"
+		:confirm-loading="modalConfirmLoading"
+		@ok="modalHandleOk"
+	>
+		<a-form
+			:model="doc"
+			:label-col="{ span: 4 }"
+			:wrapper-col="wrapperCol"
+		>
+			<a-form-item label="名称">
+				<a-input v-model:value="doc.name" />
+			</a-form-item>
+			<a-form-item label="父文档">
+				<!-- <a-input v-model:value="doc.parent" /> -->
+				<a-select v-model:value="doc.parent">
+					<a-select-option :value="0">无</a-select-option>
+					<a-select-option
+						v-for="c in level"
+						:value="jack"
+						:key="c.id"
+						:disabled="c.name === doc.name"
+					>
+						{{ c.name }}
+					</a-select-option>
+				</a-select>
+
+				<a-cascader
+					v-model:value="doc.parent"
+					:options="level"
+					:display-render="displayRender"
+					expand-trigger="hover"
+					placeholder="Please select"
+				/>
+			</a-form-item>
+			<a-form-item label="顺序">
+				<a-input v-model:value="doc.sort" />
+			</a-form-item>
+		</a-form>
+	</a-modal>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import axios from "axios";
+import { message } from "ant-design-vue";
+import { Tool } from "@/util/tool";
+
+const columns = [
+	{
+		title: "名称",
+		dataIndex: "name",
+		key: "name",
+	},
+	{
+		title: "父文档",
+		dataIndex: "parent",
+		key: "parent",
+	},
+	{
+		title: "顺序",
+		key: "sort",
+		dataIndex: "sort",
+	},
+	{
+		title: "操作",
+		key: "action",
+		slots: { customRender: "bodyCell" },
+	},
+];
+
+export default defineComponent({
+	components: {},
+	setup() {
+		const docs = ref();
+
+		const loading = ref(false);
+
+		const search = ref();
+		search.value = {};
+
+		const level = ref();
+
+		/**
+		 * 数据查询
+		 **/
+		const handleQuery = () => {
+			loading.value = true;
+			axios.get("/doc/all").then((res) => {
+				loading.value = false;
+				const data = res.data;
+
+				if (data.success) {
+					docs.value = data.content;
+					console.log("初始数据: ", data.content);
+
+					level.value = [];
+					level.value = Tool.array2Tree(docs.value, 0);
+
+					console.log("树形数据: ", level);
+				} else {
+					message.error(data.message);
+				}
+			});
+		};
+
+		const doc = ref({});
+		const modalVisible = ref<boolean>(false);
+		const modalConfirmLoading = ref<boolean>(false);
+
+		const edit = (record: any) => {
+			modalVisible.value = true;
+			doc.value = Tool.copy(record);
+		};
+
+		const modalHandleOk = () => {
+			modalConfirmLoading.value = true;
+			axios.post("/doc/save", doc.value).then((res) => {
+				const data = res.data;
+
+				modalConfirmLoading.value = false;
+				if (data.success) {
+					modalVisible.value = false;
+
+					handleQuery();
+
+					message.success("保存成功！");
+				} else {
+					message.error(data.message);
+				}
+			});
+		};
+
+		const add = () => {
+			modalVisible.value = true;
+			doc.value = {};
+		};
+
+		const handleDelete = (id: any) => {
+			axios.delete("/doc/delete/" + id).then((res) => {
+				const data = res.data;
+
+				if (data.success) {
+					modalVisible.value = false;
+					modalConfirmLoading.value = false;
+
+					handleQuery();
+					message.success("删除成功！");
+				} else {
+					message.error("删除失败！");
+				}
+			});
+		};
+
+		onMounted(() => {
+			handleQuery();
+		});
+
+		return {
+			columns,
+			docs,
+			loading,
+
+			modalVisible,
+			edit,
+			doc,
+			modalHandleOk,
+			modalConfirmLoading,
+			add,
+			handleDelete,
+			search,
+			handleQuery,
+			level,
+		};
+	},
+});
+</script>
+
+<style scoped>
+img {
+	width: 50px;
+	height: 50px;
+	line-height: 50px;
+	border-radius: 8%;
+	margin: 5px 0;
+}
+</style>
