34d335228a2dcd2b61d4eb7fbfd896f7ebbdd14f
Zhiqing Zhong <1775840762@qq.com>
2022-01-09 19:43:56 +0800

    显示侧边分类导航栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,47 +3,40 @@
 		<a-layout>
 			<a-layout-sider width="200" style="background: #fff">
 				<a-menu
-					v-model:selectedKeys="selectedKeys2"
 					v-model:openKeys="openKeys"
+					v-model:selectedKeys="selectedKeys"
 					mode="inline"
-					:style="{ height: '100%', borderRight: 0 }"
+					theme="dark"
+					:inline-collapsed="collapsed"
 				>
-					<a-sub-menu key="sub1">
-						<template #title>
-							<span>
-								<user-outlined />
-								subnav 1
-							</span>
+					<a-menu-item key="welcome">
+						<router-link :to="'/'">
+							<MailOutlined />
+							<span>欢迎</span>
+						</router-link>
+					</a-menu-item>
+					<a-sub-menu v-for="item in level" :key="item.id">
+						<template v-slot:title>
+                            <span><user-outlined /> {{item.name}} </span>
 						</template>
-						<a-menu-item key="1">option1</a-menu-item>
-						<a-menu-item key="2">option2</a-menu-item>
-						<a-menu-item key="3">option3</a-menu-item>
-						<a-menu-item key="4">option4</a-menu-item>
+						
+						<a-menu-item v-for="child in item.children" :key="child.id">
+                            <MailOutlined /><span> {{child.name}} </span>
+                        </a-menu-item>
+
 					</a-sub-menu>
-					<a-sub-menu key="sub2">
-						<template #title>
-							<span>
-								<laptop-outlined />
-								subnav 2
-							</span>
+					<!-- <a-sub-menu key="sub2">
+						<template #icon>
+							<AppstoreOutlined />
 						</template>
-						<a-menu-item key="5">option5</a-menu-item>
-						<a-menu-item key="6">option6</a-menu-item>
-						<a-menu-item key="7">option7</a-menu-item>
-						<a-menu-item key="8">option8</a-menu-item>
-					</a-sub-menu>
-					<a-sub-menu key="sub3">
-						<template #title>
-							<span>
-								<notification-outlined />
-								subnav 3
-							</span>
-						</template>
-						<a-menu-item key="9">option9</a-menu-item>
-						<a-menu-item key="10">option10</a-menu-item>
-						<a-menu-item key="11">option11</a-menu-item>
-						<a-menu-item key="12">option12</a-menu-item>
-					</a-sub-menu>
+						<template #title>Navigation Two</template>
+						<a-menu-item key="9">Option 9</a-menu-item>
+						<a-menu-item key="10">Option 10</a-menu-item>
+						<a-sub-menu key="sub3" title="Submenu">
+							<a-menu-item key="11">Option 11</a-menu-item>
+							<a-menu-item key="12">Option 12</a-menu-item>
+						</a-sub-menu>
+					</a-sub-menu> -->
 				</a-menu>
 			</a-layout-sider>
 			<a-layout-content
@@ -86,6 +79,8 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue"; // @ is an alias to /src
 import axios from "axios";
+import { Tool } from "@/util/tool";
+import { message } from "ant-design-vue";
 
 export default defineComponent({
 	name: "Home",
@@ -106,7 +101,35 @@ export default defineComponent({
 			{ type: "MessageOutlined", text: "2" },
 		];
 
+		const level = ref();
+		level.value = [];
+		/**
+		 * 分类数据查询
+		 **/
+		const handleQueryCategory = () => {
+			// loading.value = true;
+			axios.get("/category/all").then((res) => {
+				// loading.value = false;
+				const data = res.data;
+
+				if (data.success) {
+					var categorys = data.content;
+					console.log("初始数据: ", data.content);
+
+					// level.value = [];
+					level.value = Tool.array2Tree(categorys, 0);
+
+					console.log("树形数据: ", level);
+
+					// 目录查询完成之后再进行电子书的渲染
+				} else {
+					message.error(data.message);
+				}
+			});
+		};
+
 		onMounted(() => {
+            handleQueryCategory();
 			axios
 				.get("/ebook/list", {
 					params: {
@@ -124,6 +147,9 @@ export default defineComponent({
 			ebooks,
 			pagination,
 			actions,
+
+			handleQueryCategory,
+			level,
 		};
 	},
 });
