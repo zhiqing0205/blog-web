8e5a187afdaec9920f04e1a4f068cc51c2f6f37b
Zhiqing Zhong <1775840762@qq.com>
2022-01-11 23:40:15 +0800

    右侧显示富文本内容

--- src/views/doc.vue
+++ src/views/doc.vue
@@ -4,7 +4,7 @@
  * @Author: Zhiqing Zhong
  * @Date: 2021-11-06 19:33:53
  * @LastEditors: Zhiqing Zhong
- * @LastEditTime: 2022-01-11 19:30:51
+ * @LastEditTime: 2022-01-11 23:37:58
 -->
 <template>
 	<a-layout>
@@ -19,14 +19,19 @@
 			>
 				<a-row :gutter="24">
 					<a-col :span="8">
-						<a-tree 
-                        v-if="level.length > 0" 
-                        class="tree" block-node 
-                        :tree-data="level"
-                        :replaceFields="{ title: 'name', key: 'id', value: 'id' }"
-                        :defaultExpandAll= "true" />
+						<a-tree
+							v-if="level.length > 0"
+							class="tree"
+							block-node
+							:tree-data="level"
+							@select="onSelect"
+							:replaceFields="{ title: 'name', key: 'id', value: 'id' }"
+							:defaultExpandAll="true"
+						/>
 					</a-col>
-					<a-col :span="16"> </a-col>
+					<a-col :span="16">
+						<div class="wangeditor" v-html="html"></div>
+					</a-col>
 				</a-row>
 			</a-layout-content>
 		</a-layout>
@@ -43,8 +48,7 @@ import { useRouter } from "vue-router";
 export default defineComponent({
 	components: {},
 	setup() {
-
-        const route1 = useRouter();
+		const route1 = useRouter();
 		const route = route1.currentRoute;
 
 		const doc = ref();
@@ -53,6 +57,8 @@ export default defineComponent({
 
 		const level = ref();
 		level.value = [];
+
+		const html = ref();
 
 		/**
 		 * 数据查询
@@ -67,13 +73,38 @@ export default defineComponent({
 					console.log("初始数据: ", data.content);
 
 					level.value = [];
-					level.value = Tool.array2Tree(data.content, '0');
+					level.value = Tool.array2Tree(data.content, "0");
 
-                    console.log("level", level.value);
+					console.log("level", level.value);
 				} else {
 					message.error(data.message);
 				}
 			});
+		};
+
+		/**
+		 * 内容数据查询
+		 **/
+		const handleQueryContent = (id: string) => {
+			loading.value = true;
+			axios.get("/doc/content/" + id).then((res) => {
+				loading.value = false;
+				const data = res.data;
+
+				if (data.success) {
+					html.value = data.content;
+					console.log(html.value);
+				} else {
+					message.error(data.message);
+				}
+			});
+		};
+
+		const onSelect = (selectedKeys: any, info: any) => {
+			console.log("select", selectedKeys, info);
+			if (Tool.isNotEmpty(selectedKeys)) {
+				handleQueryContent(selectedKeys[0]);
+			}
 		};
 
 		onMounted(() => {
@@ -84,7 +115,78 @@ export default defineComponent({
 			loading,
 			handleQuery,
 			level,
+			html,
+			onSelect,
 		};
 	},
 });
 </script>
+
+<style lang="less">
+.wangeditor {
+	p,
+	li,
+	td,
+	th,
+	blockquote {
+		white-space: pre-wrap; /* 显示空格 */
+	}
+
+	/* 表格 */
+	table {
+		border-collapse: collapse;
+	}
+	table th,
+	table td {
+		border: 1px solid #ccc;
+		min-width: 50px;
+		height: 20px;
+		text-align: left;
+	}
+	table th {
+		background-color: #f1f1f1;
+		text-align: center;
+	}
+
+	/* 代码块 */
+	pre > code {
+		display: block;
+		border: 1px solid hsl(0, 0%, 91%);
+		border-radius: 4px 4px;
+		text-indent: 0;
+		background-color: #fafafa;
+		padding: 10px;
+		font-size: 14px;
+	}
+
+	/* 引用 */
+	blockquote {
+		display: block;
+		border-left: 8px solid #d0e5f2;
+		padding: 10px 10px;
+		margin: 10px 0;
+		background-color: #f1f1f1;
+	}
+
+	/* 列表 */
+	ul,
+	ol {
+		margin: 10px 0 10px 20px;
+	}
+
+	/* 分割线 */
+	hr {
+		display: block;
+		width: 90%;
+		margin: 20px auto;
+		border: 0;
+		height: 1px;
+		background-color: #ccc;
+	}
+
+	img {
+		max-width: 100%;
+	}
+}
+</style>
+>
