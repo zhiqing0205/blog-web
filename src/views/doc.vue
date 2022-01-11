35f6d18c6c515f016ecb21cbcfeb9522e8c247e3
Zhiqing Zhong <1775840762@qq.com>
2022-01-11 19:15:37 +0800

    增加文档树形展示，遗留查询当前电子书下的文档bug

--- src/views/doc.vue
+++ src/views/doc.vue
@@ -4,7 +4,7 @@
  * @Author: Zhiqing Zhong
  * @Date: 2021-11-06 19:33:53
  * @LastEditors: Zhiqing Zhong
- * @LastEditTime: 2022-01-11 18:56:57
+ * @LastEditTime: 2022-01-11 19:14:27
 -->
 <template>
 	<a-layout>
@@ -17,10 +17,69 @@
 					minHeight: '280px',
 				}"
 			>
-				<div class="about">
-					<h1>This is an doc page</h1>
-				</div>
+				<a-row :gutter="24">
+					<a-col :span="8">
+						<a-tree 
+                        v-if="level.length > 0" 
+                        class="tree" block-node 
+                        :tree-data="level"
+                        :replaceFields="{ title: 'name', key: 'id', value: 'id' }"
+                        :defaultExpandAll= "true" />
+					</a-col>
+					<a-col :span="16"> </a-col>
+				</a-row>
 			</a-layout-content>
 		</a-layout>
 	</a-layout>
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import axios from "axios";
+import { message } from "ant-design-vue";
+import { Tool } from "@/util/tool";
+
+export default defineComponent({
+	components: {},
+	setup() {
+		const doc = ref();
+
+		const loading = ref(false);
+
+		const level = ref();
+		level.value = [];
+
+		/**
+		 * 数据查询
+		 **/
+		const handleQuery = () => {
+			loading.value = true;
+			axios.get("/doc/all").then((res) => {
+				loading.value = false;
+				const data = res.data;
+
+				if (data.success) {
+					console.log("初始数据: ", data.content);
+
+					level.value = [];
+					level.value = Tool.array2Tree(data.content, 0);
+
+                    console.log("level", level.value);
+				} else {
+					message.error(data.message);
+				}
+			});
+		};
+
+		onMounted(() => {
+			handleQuery();
+		});
+
+		return {
+			loading,
+			handleQuery,
+			level,
+		};
+	},
+});
+</script>
