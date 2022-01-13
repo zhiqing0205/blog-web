476d45caa84ea776e6dc4bcffe5a857dcfc47e4c
Zhiqing Zhong <1775840762@qq.com>
2022-01-13 17:31:12 +0800

    修改菜单为flex布局，增加登录功能

--- src/components/the-header.vue
+++ src/components/the-header.vue
@@ -4,39 +4,116 @@
  * @Author: Zhiqing Zhong
  * @Date: 2021-11-06 23:44:19
  * @LastEditors: Zhiqing Zhong
- * @LastEditTime: 2022-01-12 10:18:20
+ * @LastEditTime: 2022-01-13 17:29:31
 -->
 
 <template>
 	<a-layout-header class="header">
 		<div class="logo" />
-		<a-menu
-			v-model:selectedKeys="selectedKeys1"
-			theme="dark"
-			mode="horizontal"
-			:style="{ lineHeight: '64px' }"
-		>
-			<a-menu-item key="/"><router-link to="/">首页</router-link></a-menu-item>
-            <a-menu-item key="/admin/user"
-				><router-link to="/admin/user">用户管理</router-link></a-menu-item
+		<a-row>
+			<a-col :xs="20" :sm="18" :md="16" :lg="14" :xl="12">
+				<a-menu theme="dark" mode="horizontal" class="menu">
+					<a-menu-item key="/"
+						><router-link to="/">首页</router-link></a-menu-item
+					>
+					<a-menu-item key="/admin/user">
+						<router-link to="/admin/user">用户管理</router-link>
+					</a-menu-item>
+					<a-menu-item key="/admin/ebook">
+						<router-link to="/admin/ebook">电子书管理</router-link>
+					</a-menu-item>
+
+					<a-menu-item key="/admin/category">
+						<router-link to="/admin/category">分类管理</router-link>
+					</a-menu-item>
+					<a-menu-item key="/about">
+						<router-link to="/about">关于我们</router-link>
+					</a-menu-item>
+				</a-menu></a-col
 			>
-			<a-menu-item key="/admin/ebook"
-				><router-link to="/admin/ebook">电子书管理</router-link></a-menu-item
-			>
-			<a-menu-item key="/admin/category"
-				><router-link to="/admin/category">分类管理</router-link></a-menu-item
-			>
-			<a-menu-item key="/about"
-				><router-link to="/about">关于我们</router-link></a-menu-item
-			>
-		</a-menu>
+			<a-col :xs="4" :sm="6" :md="8" :lg="10" :xl="12">
+				<a class="login-menu" @click="showLoginModal"> 登录 </a>
+			</a-col>
+		</a-row>
 	</a-layout-header>
+
+	<a-modal
+		v-model:visible="modalVisible"
+		title="登录"
+		:confirm-loading="modalConfirmLoading"
+		@ok="login"
+	>
+		<a-form :model="user" :label-col="{ span: 4 }">
+			<a-form-item label="登录名">
+				<a-input v-model:value="user.loginName" :disabled="!!user.id" />
+			</a-form-item>
+			<a-form-item label="密码">
+				<a-input-password v-model:value="user.password" />
+			</a-form-item>
+		</a-form>
+	</a-modal>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import axios from "axios";
+import { message } from "ant-design-vue";
+import { Tool } from "@/util/tool";
+declare let hexMd5: any;
+declare let KEY: any;
+
 
 export default defineComponent({
 	name: "the-header",
+
+	setup() {
+		const user = ref();
+		user.value = {};
+		const modalVisible = ref<boolean>(false);
+		const modalConfirmLoading = ref<boolean>(false);
+
+		const showLoginModal = () => {
+			modalVisible.value = true;
+		};
+
+		const login = () => {
+            if(Tool.isEmpty(user.value.loginName) || Tool.isEmpty(user.value.password)) {
+                message.error("登录名或密码为空！");
+                return;
+            }
+            modalConfirmLoading.value = true;
+            user.value.password = hexMd5(user.value.password + KEY);
+			axios.post("/user/login", user.value)
+				.then((res) => {
+					modalConfirmLoading.value = false;
+					const data = res.data;
+
+					if (data.success) {
+                        modalVisible.value = false;
+                        user.value = {};
+						message.success("登录成功！");
+					} else {
+                        user.value.password = null;
+						message.error(data.message);
+					}
+				});
+        };
+
+		return {
+			user,
+			modalVisible,
+			modalConfirmLoading,
+			showLoginModal,
+            login
+		};
+	},
 });
 </script>
+
+<style lang="less">
+.login-menu {
+	float: right !important;
+	padding-left: 10px;
+	color: white;
+}
+</style>
