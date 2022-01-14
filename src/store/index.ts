3b6cc726427fe70fc71b81f359b3a747f317c9a6
Zhiqing Zhong <1775840762@qq.com>
2022-01-14 11:44:37 +0800

    引入session-storage，缓存用户信息并接入vuex使用

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,17 +4,21 @@
  * @Author: Zhiqing Zhong
  * @Date: 2021-11-06 19:33:53
  * @LastEditors: Zhiqing Zhong
- * @LastEditTime: 2022-01-14 11:28:32
+ * @LastEditTime: 2022-01-14 11:39:56
  */
 import { createStore } from "vuex";
 
+declare let SessionStorage : any;
+const USER = "user";
+
 const store  = createStore({
 	state: {
-        user: {}
+        user: SessionStorage.get(USER) || {}
     },
 	mutations: {
         setUser(state, user){
             state.user = user;
+            SessionStorage.set(USER, user);
         }
     },
 	actions: {},
